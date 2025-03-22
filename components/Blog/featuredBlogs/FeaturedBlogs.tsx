eb74113895888a8593c428d7d13e7b4bc06792d5
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-22 09:22:52 +0000

    working on the card

--- components/Blog/featuredBlogs/FeaturedBlogs.tsx
+++ components/Blog/featuredBlogs/FeaturedBlogs.tsx
@@ -1,9 +1,20 @@
 import Card from "@/components/Card/Card";
 
+import img from "@/public/assets/image1.png";
+
 function FeaturedBlogs() {
+  const data = {
+    imageSrc: img,
+    title: `surfboard shaping and 
+design behind the scenes of crafting the perfect board `,
+
+    date: "Monday, June 05, 2023",
+    price: "Prices starting at 580€",
+  };
+
   return (
     <section>
-      <Card />
+      <Card data={data} />
     </section>
   );
 }
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,6 +1,25 @@
-function Card() {
-  return (
-    <div>Card</div>
-  )
+import Image from "next/image";
+import styles from "./Card.module.scss";
+
+interface CardProps {
+  data: {
+    imageSrc: string;
+    title: string;
+    date: string;
+    price?: string;
+  };
 }
-export default Card
+
+function Card({ data }: CardProps) {
+  const { imageSrc, title, date, price } = data;
+  return (
+    <div className={styles.card}>
+      <Image src={imageSrc} alt={title} width={420} height={355} />
+      <p>{title}</p>
+
+      <small>{date}</small>
+      {price && <small>{price}</small>}
+    </div>
+  );
+}
+export default Card;
