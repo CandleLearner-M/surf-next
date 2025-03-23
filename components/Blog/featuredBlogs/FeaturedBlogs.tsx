8e156cb4cbf050619ba6560a025d1f5a3580c185
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-23 10:29:42 +0000

    working on the blog page

--- components/Blog/featuredBlogs/FeaturedBlogs.tsx
+++ components/Blog/featuredBlogs/FeaturedBlogs.tsx
@@ -2,10 +2,12 @@ import Card from "@/components/Card/Card";
 
 import img from "@/public/assets/image1.png";
 
+import styles from './FeaturedBlogs.module.scss'
+
 function FeaturedBlogs() {
   const data = {
     imageSrc: img,
-    title: `surfboard shaping and 
+    title: `Surfboard shaping and 
 design behind the scenes of crafting the perfect board `,
 
     date: "Monday, June 05, 2023",
@@ -13,8 +15,14 @@ design behind the scenes of crafting the perfect board `,
   };
 
   return (
-    <section>
-      <Card data={data} />
+    <section className={styles.blogs}>
+      <h3>Our featured articles</h3>
+      <div>
+        <Card data={data} />
+        <Card data={data} />
+        <Card data={data} />
+        <Card data={data} />
+      </div>
     </section>
   );
 }
