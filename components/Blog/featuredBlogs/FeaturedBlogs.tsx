f66c78ad49442d47fb898a69738fe9f6505ae214
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-24 07:18:17 +0000

    working on the dynamic routes

--- components/Blog/featuredBlogs/FeaturedBlogs.tsx
+++ components/Blog/featuredBlogs/FeaturedBlogs.tsx
@@ -1,23 +1,37 @@
+"use client";
+
 import Card from "@/components/Card/Card";
 
 import { Article } from "@/types/types";
 import styles from "./FeaturedBlogs.module.scss";
+import { useState } from "react";
 
 interface FeaturedBlogsProps {
   articles: Article[];
 }
 
 function FeaturedBlogs({ articles }: FeaturedBlogsProps) {
+  const [itemNumber, setItemNumber] = useState(3);
+
+  const onShowMore = function () {
+    if (itemNumber + 3 > articles.length) return setItemNumber(articles.length);
+    setItemNumber((itemNumber) => itemNumber + 3);
+  };
+
   return (
     <section className={styles.blogs}>
       <h3>Our featured articles</h3>
       <div>
-        {articles.map((item, idx) => (
+        {articles.slice(0, itemNumber).map((item, idx) => (
           <Card data={item} key={idx} />
         ))}
       </div>
 
-      <button className="btn btn--medium btn--turquoise">See More</button>
+      {itemNumber < articles.length && (
+        <button className="btn btn--medium btn--turquoise" onClick={onShowMore}>
+          See More
+        </button>
+      )}
     </section>
   );
 }
