12348156a507b1825683038723c090f9b8a7de08
Mostafa <candlelearner.m.dev@gmail.com>
2025-03-27 15:57:21 +0000

    working on posting the emails sign ups

--- components/Blog/HomeBlogs/HomeBlogs.tsx
+++ components/Blog/HomeBlogs/HomeBlogs.tsx
@@ -2,16 +2,26 @@ import { fetchBlogArticles, formatDate } from "@/utils/strapi.utils";
 import Image from "next/image";
 
 import styles from "./HomeBlogs.module.scss";
+import Link from "next/link";
 
 async function HomeBlogs() {
-  const blogs = (await fetchBlogArticles()).slice(0, 4);
+  const blogs = await fetchBlogArticles();
+
+  const highlightArticle = blogs.find((article) => article.isFeaturedArticle)!;
+  const otherBlogs = blogs
+    .filter((article) => !article.isFeaturedArticle)
+    .slice(0, 3);
 
   return (
     <section className={styles.homeblog}>
       <h2>The Blog.</h2>
       <div>
-        {blogs.map((blog) => (
-          <div key={blog.id}>
+        {[highlightArticle, ...otherBlogs].map((blog, idx) => (
+          <Link
+            href={"/blog/" + blog.slug}
+            key={blog.id}
+            className={styles[`blog${idx + 1}`]}
+          >
             <Image
               src={blog.image}
               alt={blog.headline}
@@ -20,7 +30,7 @@ async function HomeBlogs() {
             />
             <h4>{blog.headline}</h4>
             <p>{formatDate(blog.createdAt)}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
